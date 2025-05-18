882819bf09da7ade679a4142aeca2c1bb94f42a7
batuhanbozat <bozatbatu@gmail.com>
2025-05-18 20:28:47 +0300

    Gorsel Yenilemeler
    Sayfalarda görseller düzeltildi

--- web projesi/iletisim.php
+++ web projesi/iletisim.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Iletisim</title>
   <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="stiller.css">
